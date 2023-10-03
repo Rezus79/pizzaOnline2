f884aeafa2b18ee6371546c91354b09a129ab538
florent <duparquet.florent@orange.frgit config --global user.email duparquet.florent@orange.fr>
2023-10-03 16:12:14 +0200

    gestion du panier ok

--- src/main/java/fr/eni/ecole/pizzaonline/controller/CommandeController.java
+++ src/main/java/fr/eni/ecole/pizzaonline/controller/CommandeController.java
@@ -13,6 +13,7 @@ import org.springframework.web.bind.annotation.SessionAttributes;
 import fr.eni.ecole.pizzaonline.bll.ProduitService;
 import fr.eni.ecole.pizzaonline.bo.Commande;
 import fr.eni.ecole.pizzaonline.bo.DetailCommande;
+import fr.eni.ecole.pizzaonline.bo.Produit;
 
 @Controller
 @RequestMapping("/commande")
@@ -21,50 +22,83 @@ public class CommandeController {
 
 	@Autowired
 	public ProduitService produitService;
-	
+
 	@PostMapping("/menu")
-	String menu(@RequestParam Long idProduit, @RequestParam Integer quantite,Model model) {
+	String menu(@RequestParam Long idProduit, @RequestParam Integer quantite, Model model) {
 		Commande panier = (Commande) model.getAttribute("panier");
 		if (panier == null) {
 			panier = new Commande();
 			model.addAttribute("panier", panier);
 		}
-		DetailCommande dc = new DetailCommande(produitService.consulterProduitParId(idProduit),panier,quantite);
-		panier.getLstDetail().add(dc);
+//		DetailCommande dc = new DetailCommande(produitService.consulterProduitParId(idProduit), panier, quantite);
+//		if (panier.getLstDetail().isEmpty() == true) {
+//			panier.getLstDetail().add(dc);
+//		} else {
+//			for (DetailCommande dec : panier.getLstDetail()) {
+//				if (dec.getProduit().equals(produitService.consulterProduitParId(idProduit))) {
+//					dc.setQuantite(dc.getQuantite() + quantite);
+//					break;
+//				} else {
+//					panier.getLstDetail().add(dc);
+//					break;
+//				}
+//			}
+//
+//		}
 		
+		Produit produit = produitService.consulterProduitParId(idProduit);
+		DetailCommande dc = null;
+
+		// Recherchez si le produit existe déjà dans le panier
+		for (DetailCommande dec : panier.getLstDetail()) {
+		    if (dec.getProduit().equals(produit)) {
+		        dc = dec; // Le produit a été trouvé, donc assignez le détail de commande existant à dc
+		        break;
+		    }
+		}
+
+		if (dc != null) {
+		    // Le produit existe déjà dans le panier, ajoutez simplement la quantité
+		    dc.setQuantite(dc.getQuantite() + quantite);
+		} else {
+		    // Le produit n'existe pas dans le panier, créez un nouveau DetailCommande
+		    dc = new DetailCommande(produit, panier, quantite);
+		    panier.getLstDetail().add(dc);
+		}
+
 		return "redirect:/carte";
 	}
-	
+
 	@GetMapping("/panier")
 	String panier(@ModelAttribute("panier") Commande panier) {
-		
+
 		return "home/panier";
-		
+
 	}
-	
+
 	@PostMapping("/panier")
 	String panier(@ModelAttribute("panier") Commande panier, @RequestParam long idProduit) {
 		for (DetailCommande dc : panier.getLstDetail()) {
-			if(dc.getProduit().getId() == idProduit && dc.getQuantite() == 1) {
+			if (dc.getProduit().getId() == idProduit && dc.getQuantite() == 1) {
 				panier.getLstDetail().remove(dc);
 				break;
-			}else if(dc.getProduit().getId() == idProduit && dc.getQuantite() > 1) {
+			} else if (dc.getProduit().getId() == idProduit && dc.getQuantite() > 1) {
 				dc.setQuantite(dc.getQuantite() - 1);
 				break;
 			}
-			
+
 		}
 		return "redirect:/commande/panier";
-		
+
 	}
-	
+
 	@GetMapping("/valider")
 	String validerPanier(@ModelAttribute("panier") Commande panier, Model model) {
 		float total = 0;
-		for(DetailCommande dc : panier.getLstDetail()) {
+		for (DetailCommande dc : panier.getLstDetail()) {
 			total = total + dc.getProduit().getPrix() * dc.getQuantite();
 		}
-		model.addAttribute("total",total);
-		return "home/commande";	
+		model.addAttribute("total", total);
+		return "home/commande";
 	}
 }
